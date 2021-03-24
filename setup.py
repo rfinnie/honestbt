9b534a63e0cd8c362bf906435f7bc82bd2523bfe
Ryan Finnie <ryan@finnie.org>
2021-03-24 11:32:46 -0700

    Change license from GPLv2 to Mozilla Public License 2.0
    All contributions to this repository are from me, so this can be a
    unilateral license change going forward.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 setup(
     name="honestbt",
     description="Destructive tester which detects dishonest block devices",
-    license="GPLv2+",
+    license="MPL-2.0",
     platforms=["Unix"],
     author="Ryan Finnie",
     author_email="ryan@finnie.org",
